4d03d873ab941d1a60f801dbf3600fd2c03998e7
robertdenism <robert1994denis@gmail.com>
2022-11-08 21:38:33 +0100

    sin terminar adapter y main

---
+++ app/src/main/java/com/example/miniproyecto/Datos.java
@@ -0,0 +1,14 @@
+package com.example.miniproyecto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Datos {
+
+    public String ocio;
+
+    private static ArrayList<String> countries = new ArrayList<String>(Arrays.asList(new String[]{"Teatro","Conciertos","Deportes","Museos"};
+
+
+
+}
--- app/src/main/java/com/example/miniproyecto/LiberiIndex.java
+++ app/src/main/java/com/example/miniproyecto/LiberiIndex.java
@@ -1,10 +1,17 @@
 package com.example.miniproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class LiberiIndex extends AppCompatActivity {
+
+    RecyclerView reyclerViewUser;
+    Button mas;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,21 @@ public class LiberiIndex extends AppCompatActivity {
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
+        }
+
+            reyclerViewUser = (RecyclerView) findViewById(R.id.recycledViewprincipal);
+
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            reyclerViewUser.setHasFixedSize(true);
+
+            // use a linear layout manager
+            reyclerViewUser.setLayoutManager(new LiberiIndex(this));
+
+            // specify an adapter with the list to show
+            mAdapter = new UserAdapter(getData());
+            reyclerViewUser.setAdapter(mAdapter);
+
         }
 
     }
